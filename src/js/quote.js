5f40f970edac0d019af702c0d8084d5583a131f5
KimJangHyeok <48301140+d0orHyeok@users.noreply.github.com>
2021-10-14 22:59:11 +0900

    Add loading screen

--- src/js/quote.js
+++ src/js/quote.js
@@ -3,23 +3,38 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const googleBtn = document.getElementById("google");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 
 let apiQuotes = [];
 
+// Loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
 // Show Quote
 function newQuote() {
+    loading();
     // apiQuote 배열에서 랜덤으로 한개 뽑아오기
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     // Author가 비어있을 경우 "Unknown"
     !quote.author ? (authorText.textContent = "Unknown") : (authorText.textContent = quote.author);
     // Quote의 길이에 따른 스타일 설정
     quote.text.length > 100 ? quoteText.classList.add("long-quote") : quoteText.classList.remove("long-quote");
-
+    // 인용문 로딩 완료시 로더 hide
     quoteText.textContent = quote.text;
+    complete();
 }
 
 // API로부터 Quote 가져오기
 async function getQuotes() {
+    loading();
     const apiUrl = "https://type.fit/api/quotes";
     try {
         const response = await fetch(apiUrl);
