108b5a3ef659dbb083cca35526349d0a56628976
KimJangHyeok <48301140+d0orHyeok@users.noreply.github.com>
2021-10-15 04:02:45 +0900

    로딩 및 다음버튼 클릭시 화면에 API 데이터 출력

--- src/js/ani-quote.js
+++ src/js/ani-quote.js
@@ -1,7 +1,18 @@
+const animeTitle = document.querySelector(".anime-title");
+const chracterName = document.querySelector(".anime-character");
+const animeQuote = document.querySelector(".anime-text__quote");
+const newQuoteBtn = document.querySelector(".btn.new-quote");
+
 let apiData = {};
 
 // 화면에 가져온 데이터 출력
-function showData() {}
+function showData() {
+    animeTitle.textContent = apiData.anime;
+    chracterName.textContent = `- ${apiData.character} -`;
+    // Quote의 길이에 따른 스타일 설정
+    apiData.quote.length > 100 ? animeQuote.classList.add("long-quote") : animeQuote.classList.remove("long-quote");
+    animeQuote.textContent = apiData.quote;
+}
 
 // API로부터 제목, 캐릭터, 대사 가져오기
 async function getData() {
@@ -9,16 +20,20 @@ async function getData() {
     try {
         const response = await fetch(apiUrl);
         apiData = await response.json();
-        console.log(apiData);
     } catch (error) {
-        // Error: load localQuotes
+        // Error: dummy
         apiData = {
             anime: "Unknown",
             character: "Unkown",
             quote: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum, voluptate!",
         };
+    } finally {
+        showData();
     }
 }
+
+// Event Listener
+newQuoteBtn.addEventListener("click", getData);
 
 //On Load
 getData();
