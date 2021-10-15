d59dc4d53313455048f1da3262149c35709e72fc
JangHyeok Kim <48301140+d0orHyeok@users.noreply.github.com>
2021-10-15 15:16:35 +0900

    Refactoring func displayPhotos

--- infinite-scroll/src/js/infinite-scroll.js
+++ infinite-scroll/src/js/infinite-scroll.js
@@ -8,19 +8,30 @@ const count = 4;
 const apiKey = "ype2pjyfZXqGwhrrD9CpBKDFe3OkwEdf2Ju4Kqbe-Ig";
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+// Helper function to Set Attributes on DOM Elements
+function setAttribute(element, attributes) {
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+}
+
 // Create Elements For Links & Phothos, Add to DOM
 function displayPhotos() {
     // Run function for each object in photosArray
     photosArray.forEach((photo) => {
         // Create <a> to link oto Unsplash
         const item = document.createElement("a");
-        item.setAttribute("href", photo.links.html);
-        item.setAttribute("target", "_blank");
+        setAttribute(item, {
+            href: photo.links.html,
+            target: "_blank",
+        });
         // Create <img> for photo
         const img = document.createElement("img");
-        img.setAttribute("src", photo.urls.regular);
-        img.setAttribute("alt", photo.alt_description);
-        img.setAttribute("title", photo.alt_description);
+        setAttribute(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description,
+        });
         // Put <img> inside <a>, then put insdie imageContainer Element
         item.appendChild(img);
         imageContainer.appendChild(item);
@@ -34,7 +45,7 @@ async function getPhotos() {
         photosArray = await response.json();
         displayPhotos();
     } catch (error) {
-        console.log("error");
+        console.log(error);
     }
 }
 
