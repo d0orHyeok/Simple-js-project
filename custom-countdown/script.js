493d5d367bad73477c9ead004f42235f3c6f3af9
KimJangHyeok <48301140+d0orHyeok@users.noreply.github.com>
2021-10-17 02:22:39 +0900

    Add : save & load countdown to use local strage

--- custom-countdown/script.js
+++ custom-countdown/script.js
@@ -10,6 +10,7 @@ let countdownTitle = "";
 let countdownDate = "";
 let countdownValue = Date;
 let countdownActive;
+let savedCountdown;
 
 const second = 1000;
 const minute = second * 60;
@@ -64,6 +65,11 @@ function updateCountdown(e) {
     e.preventDefault();
     countdownTitle = e.srcElement[0].value;
     countdownDate = e.srcElement[1].value;
+    savedCountdown = {
+        title: countdownTitle,
+        date: countdownDate,
+    };
+    localStorage.setItem("countdown", JSON.stringify(savedCountdown));
     // Check for valid date
     if (countdownDate === "") {
         alert("Please select a date for the countdown.");
@@ -85,9 +91,25 @@ function resetCountdown() {
     // Reset values
     countdownTitle = "";
     countdownDate = "";
+    localStorage.removeItem("countdown");
+}
+
+function restorePreviousCountdown() {
+    // Get countdown from localStorage if available
+    if (localStorage.getItem("countdown")) {
+        inputContainer.hidden = true;
+        savedCountdown = JSON.parse(localStorage.getItem("countdown"));
+        countdownTitle = savedCountdown.title;
+        countdownDate = savedCountdown.date;
+        countdownValue = new Date(countdownDate).getTime();
+        updateDOM();
+    }
 }
 
 // Event Listener
 countdownForm.addEventListener("submit", updateCountdown);
 countdownBtn.addEventListener("click", resetCountdown);
 completeBtn.addEventListener("click", resetCountdown);
+
+// On Load
+restorePreviousCountdown();
