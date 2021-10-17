15a319a230c2b5339aa567617239fca82010ef22
JangHyeok Kim <48301140+d0orHyeok@users.noreply.github.com>
2021-10-17 16:50:10 +0900

    Add progress-bar & volume-bar event

--- video-player/script.js
+++ video-player/script.js
@@ -5,12 +5,14 @@ const playBtn = document.getElementById("play-btn");
 const volumeIcon = document.getElementById("volume-icon");
 const volumeRange = document.querySelector(".volume-range");
 const volumeBar = document.querySelector(".volume-bar");
-const currentTime = document.querySelector("time-elapsed");
+const currentTime = document.querySelector(".time-elapsed");
 const duration = document.querySelector(".time-duration");
 const fullscreenBtn = document.querySelector(".fullscreen");
 
-// Play & Pause ----------------------------------- //
+let videoCurrentTime = 0;
+let videoDuration = 0;
 
+// Play & Pause ----------------------------------- //
 const showPlayIcon = () => {
     playBtn.classList.replace("fa-pause", "fa-play");
     playBtn.setAttribute("title", "Play");
@@ -27,14 +29,65 @@ const togglePlay = () => {
     }
 };
 
-playBtn.addEventListener("click", togglePlay);
-video.addEventListener("click", togglePlay);
-video.addEventListener("ended", showPlayIcon);
-
 // Progress Bar ---------------------------------- //
 
+// Calculate dispaly time format
+const displayTime = (time) => {
+    const minutes = Math.floor(time / 60);
+    let seconds = Math.floor(time % 60);
+    seconds = seconds < 10 ? `0${seconds}` : seconds;
+    return `${minutes}:${seconds}`;
+};
+
+// Update progress bar as video plays
+const updateProgress = () => {
+    progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`;
+    currentTime.textContent = `${displayTime(video.currentTime)} /`;
+    duration.textContent = `${displayTime(video.duration)}`;
+};
+
+// Click to seek within the wideo
+const setProgress = (e) => {
+    console.log(e);
+    const newTime = e.offsetX / progressRange.offsetWidth;
+    progressBar.style.width = `${newTime * 100}%`;
+    video.currentTime = newTime * video.duration;
+};
+
 // Volume Controls --------------------------- //
+
+// Volume Bar
+const changeVolume = (e) => {
+    let volume = e.offsetX / volumeRange.offsetWidth;
+    // Rounding volume up or down
+    if (volume < 0.1) {
+        volume = 0;
+    }
+    if (volume > 0.9) {
+        volume = 1;
+    }
+    volumeBar.style.width = `${volume * 100}%`;
+    video.volume = volume;
+    // Change Icon depending on volume
+    volumeIcon.className = "";
+    if (volume > 0.7) {
+        volumeIcon.classList.add("fas", "fa-volume-up");
+    } else if (volume < 0.7 && volume > 0) {
+        volumeIcon.classList.add("fas", "fa-volume-down");
+    } else if (volume === 0) {
+        volumeIcon.classList.add("fas", "fa-volume-off");
+    }
+};
 
 // Change Playback Speed -------------------- //
 
 // Fullscreen ------------------------------- //
+
+// Event Listener
+playBtn.addEventListener("click", togglePlay);
+video.addEventListener("click", togglePlay);
+video.addEventListener("ended", showPlayIcon);
+video.addEventListener("timeupdate", updateProgress);
+video.addEventListener("canplay", updateProgress);
+progressRange.addEventListener("click", setProgress);
+volumeRange.addEventListener("click", changeVolume);
