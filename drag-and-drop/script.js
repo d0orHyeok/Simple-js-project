608372e0f348e6b4edd0abc3674af94b508fb1e3
JangHyeok Kim <48301140+d0orHyeok@users.noreply.github.com>
2021-10-20 02:56:11 +0900

    Add: save item data in localStorage

---
+++ drag-and-drop/script.js
@@ -0,0 +1,48 @@
+const addBtns = document.querySelectorAll(".add-btn:not(.solid)");
+const saveItemBtns = document.querySelectorAll(".solid");
+const addItemContainers = document.querySelectorAll(".add-container");
+const addItems = document.querySelectorAll(".add-item");
+// Item Lists
+const itemLists = document.querySelectorAll(".drag-item-list");
+const backlogList = document.getElementById("backlog-list");
+const progressList = document.getElementById("progress-list");
+const completeList = document.getElementById("complete-list");
+const onHoldList = document.getElementById("on-hold-list");
+
+// Items
+
+// Initialize Arrays
+let backlogListArray = [];
+let progressListArray = [];
+let completeListArray = [];
+let onHoldListArray = [];
+let listArrays = [];
+
+// Drag Functionality
+
+// Get Arrays from localStorage if available, set default values if not
+function getSavedColumns() {
+    if (localStorage.getItem("backlogItems")) {
+        backlogListArray = JSON.parse(localStorage.backlogItems);
+        progressListArray = JSON.parse(localStorage.progressItems);
+        completeListArray = JSON.parse(localStorage.completeItems);
+        onHoldListArray = JSON.parse(localStorage.onHoldItems);
+    } else {
+        backlogListArray = ["Release the course", "Sit back and relax"];
+        progressListArray = ["Work on projects", "Listen to music"];
+        completeListArray = ["Being cool", "Getting stuff done"];
+        onHoldListArray = ["Being uncool"];
+    }
+}
+
+// Set localStorage Arrays
+function updateSavedColumns() {
+    listArrays = [backlogListArray, progressListArray, completeListArray, onHoldListArray];
+    const arrayNames = ["backlog", "progress", "complete", "onHold"];
+    arrayNames.forEach((arrayName, index) => {
+        localStorage.setItem(`${arrayName}Items`, JSON.stringify(listArrays[index]));
+    });
+}
+
+getSavedColumns();
+updateSavedColumns();
